7e3447bb3e46f70dca3d31c2c5f9a3562983bae9
lm101845 <1018457683@qq.com>
2021-08-10 05:33:10 +0800

    08-10

---
+++ cloudmusic/src/App.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: liming
+ * @Date: 2021-08-10 03:43:56
+ * @LastEditTime: 2021-08-10 05:30:21
+ * @FilePath: \Vue2CloudMusic\cloudmusic\src\App.vue
+-->
+<template>
+  <div id="app">
+    你好
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+    // @import "./common/stylus/variable"
+    @import "common/stylus/variable"
+    // 给common起别名，这样就可以使用绝对路径了
+    #app
+        color:$color-theme
+</style>
---
+++ cloudmusic/src/router/index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: liming
+ * @Date: 2021-08-10 03:43:56
+ * @LastEditTime: 2021-08-10 04:24:47
+ * @FilePath: \Vue2CloudMusic\cloudmusic\src\router\index.js
+ */
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+    }
+  ]
+})
